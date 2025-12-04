a348079454b99c19e25e414d48913fe6ae5e02ef
th <crfthiago02@gmail.com>
2025-12-04 15:52:09 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Início</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="view/cadastrarPaciente.php">Cadastrar Paciente</a></li>
+            <li><a href="view/consultarPaciente.php">Consultar Paciente</a></li>
+        </ul>
+    </nav>
+
+    <h1><?php echo "Bem Vindo: SisCadPaciente"; ?></h1>
+
+    <br><br>
+    
+</body>
+</html>
